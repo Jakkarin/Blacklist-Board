85d3b0b3a7d185f85481f6f56e54419bcc2b7799
Jakkarin <Jakkarinwebmaster@gmail.com>
2015-05-26 18:57:48 +0700

    Add CodeIgniter

---
+++ settings.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| DATABASE
+|--------------------------------------------------------------------------
+|
+| default is PDO
+|
+*/
+define('DB_HOSTNAME','localhost');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
+define('DB_DBNAME','blacklist');
+define('DB_PERFIX','bl_');
+/*
+|--------------------------------------------------------------------------
+| Templates Directory Path
+|--------------------------------------------------------------------------
+|
+| default is default template
+| bl-content/template/*.  Use a path without trailing slash.
+|
+*/
+define('APP_TEMPLATE','default');
