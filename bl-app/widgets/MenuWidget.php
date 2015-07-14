dcd2ff3046aa9d623d23e4174b2b56fa0701c707
Jakkarin <Jakkarinwebmaster@gmail.com>
2015-07-14 16:52:17 +0700

    add cache with cache_remeber

--- bl-app/widgets/MenuWidget.php
+++ bl-app/widgets/MenuWidget.php
@@ -5,24 +5,27 @@ class MenuWidget
 {
 	public function run() {
 		$bl =& get_instance();
-		$bl->load->model('menu_model');
-		$data = $bl->menu_model->all();
-		$a1 = [];
-		$a2 = [];
-		$a3 = [];
-		$c2 = [];
-		$c3 = [];
-		foreach ($data as $k => $v) {
-			if ($v->type == 0) {
-				$a1[] = (array) $v;
-			} elseif ($v->type == 1) {
-				$a2[] = (array) $v;
-				$c2[] = $v->sub;
-			} elseif ($v->type == 2) {
-				$a3[] = (array) $v;
-				$c3[] = $v->sub;
+		$data = $bl->output->cache_remember('widget.menu', 60, function() use ($bl) {
+			$bl->load->model('menu_model');
+			$data = $bl->menu_model->all();
+			$a1 = [];
+			$a2 = [];
+			$a3 = [];
+			$c2 = [];
+			$c3 = [];
+			foreach ($data as $k => $v) {
+				if ($v->type == 0) {
+					$a1[] = (array) $v;
+				} elseif ($v->type == 1) {
+					$a2[] = (array) $v;
+					$c2[] = $v->sub;
+				} elseif ($v->type == 2) {
+					$a3[] = (array) $v;
+					$c3[] = $v->sub;
+				}
 			}
-		}
-		$bl->load->view('widgets/menu', compact('a1','a2','a3','c2','c3'));
+			return compact('a1','a2','a3','c2','c3');
+		});
+		$bl->load->view('widgets/menu', $data);
 	}
 }
